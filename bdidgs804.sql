04690ca3ca93e20903ccf88f061a53562ba581fc
unknown <c4p9jyqcsm@privaterelay.appleid.com>
2026-03-13 18:35:11 -0600

    ABC Escuela Terminado

--- bdidgs804.sql
+++ bdidgs804.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.45, for macos15 (arm64)
+-- MySQL dump 10.13  Distrib 8.0.43, for Win64 (x86_64)
 --
 -- Host: localhost    Database: bdidgs804
 -- ------------------------------------------------------
--- Server version	8.0.44
+-- Server version	8.0.43
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -53,7 +53,7 @@ CREATE TABLE `alumnos` (
   `apellidos` varchar(200) DEFAULT NULL,
   `telefono` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -65,6 +65,91 @@ LOCK TABLES `alumnos` WRITE;
 INSERT INTO `alumnos` VALUES (1,'Diego Alejandro','diego@gmail.com','2026-02-14 09:20:26','gonzalez gaytan','4771230033'),(2,'Roger','rogelio@gmail.com','2026-02-14 09:20:26','echeveste mendiola','123456666'),(3,'Uriel','uri@gmail.com','2026-02-14 09:20:26','Hernandez Camacho','98773764'),(4,'diego','gonz@gmail.com','2026-02-20 17:55:44','gonz gat','273666464');
 /*!40000 ALTER TABLE `alumnos` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `cursos`
+--
+
+DROP TABLE IF EXISTS `cursos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cursos` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(150) NOT NULL,
+  `descripcion` text,
+  `maestro_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `maestro_id` (`maestro_id`),
+  CONSTRAINT `cursos_ibfk_1` FOREIGN KEY (`maestro_id`) REFERENCES `maestros` (`matricula`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cursos`
+--
+
+LOCK TABLES `cursos` WRITE;
+/*!40000 ALTER TABLE `cursos` DISABLE KEYS */;
+INSERT INTO `cursos` VALUES (1,'Programación','Programación OO',122),(2,'UI','Mejora tus diseños',122),(3,'Desarrollo Web Profesional','Crea paginas profesionales',123),(4,'Progr','adsfsdf',33),(5,'UX','nose',33),(6,'Hola','Hola',35);
+/*!40000 ALTER TABLE `cursos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `inscripciones`
+--
+
+DROP TABLE IF EXISTS `inscripciones`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `inscripciones` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `alumno_id` int NOT NULL,
+  `curso_id` int NOT NULL,
+  `fecha_incripcion` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uq_alumno_curso` (`alumno_id`,`curso_id`),
+  KEY `curso_id` (`curso_id`),
+  CONSTRAINT `inscripciones_ibfk_1` FOREIGN KEY (`alumno_id`) REFERENCES `alumnos` (`id`),
+  CONSTRAINT `inscripciones_ibfk_2` FOREIGN KEY (`curso_id`) REFERENCES `cursos` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `inscripciones`
+--
+
+LOCK TABLES `inscripciones` WRITE;
+/*!40000 ALTER TABLE `inscripciones` DISABLE KEYS */;
+INSERT INTO `inscripciones` VALUES (1,1,2,'2026-03-07 07:15:00'),(3,1,3,'2026-03-07 08:10:43'),(4,1,1,'2026-03-13 12:56:16');
+/*!40000 ALTER TABLE `inscripciones` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `maestros`
+--
+
+DROP TABLE IF EXISTS `maestros`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `maestros` (
+  `matricula` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) DEFAULT NULL,
+  `apellidos` varchar(50) DEFAULT NULL,
+  `especialidad` varchar(50) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`matricula`)
+) ENGINE=InnoDB AUTO_INCREMENT=124 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `maestros`
+--
+
+LOCK TABLES `maestros` WRITE;
+/*!40000 ALTER TABLE `maestros` DISABLE KEYS */;
+INSERT INTO `maestros` VALUES (1,'juan ','dominguez','quimica','sdfsdf@gmail.com'),(33,'Dario','dddddd','Wev','dario@gmail.com'),(34,'jjfhghfjd','jfjfjffj','wdnnc ','ififkv@gmail.com'),(35,'Dario','Man','adsasd','asdad@gmail.com'),(122,'Bartolo','Gonzalez','Programacion','bartprofe@gmail.com'),(123,'Profe Cardiel','Cardiel','Desarrollo Web Profesional','cardiel@gmail.com');
+/*!40000 ALTER TABLE `maestros` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -75,4 +160,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-02-21  9:49:07
+-- Dump completed on 2026-03-13 18:34:15
